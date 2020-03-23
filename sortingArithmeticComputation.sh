2fc22cc07a24df41698e068e47838111c8104f48
pranaliandre <pranaliandre06@gmail.com>
2020-03-23 13:45:07 -0500

    Added: Calculating a+b*c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -4,3 +4,6 @@ echo "Welcome"
 read -p "Enter the first number: " number1
 read -p "Enter the second number: " number2
 read -p "Enter the third number: " number3
+
+#compute
+result1=`echo "scale=2; $((number1+number2*number3))" | bc`
