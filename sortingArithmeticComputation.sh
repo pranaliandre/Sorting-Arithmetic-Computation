06d8a90553062a2a70935ce40b55e2c5d0c8c311
pranaliandre <pranaliandre06@gmail.com>
2020-03-23 13:36:40 -0500

    Added: Take a three input from user

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,2 +1,6 @@
+#!/bin/bash -x
 echo "Welcome"
-
+#Take three input from user
+read -p "Enter the first number: " number1
+read -p "Enter the second number: " number2
+read -p "Enter the third number: " number3
