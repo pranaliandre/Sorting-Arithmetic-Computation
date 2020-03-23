60c0772990ac3fe3dc5bc50b24c9499793ebd703
pranaliandre <pranaliandre06@gmail.com>
2020-03-23 14:14:35 -0500

    Added: Calculating c+a/b

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -8,5 +8,8 @@ read -p "Enter the third number: " number3
 #compute
 result1=`echo "scale=2; $((number1+number2*number3))" | bc`
 
-#comput a*b+c
+#compute a*b+c
 result2=`echo "scale=2; $((number1*number2+number3))" | bc`
+
+#compute c+a/b
+result3=`echo "scale=2; $((number3+number1/number2))" | bc`
